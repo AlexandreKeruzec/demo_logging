3c6689736ab01a7fb7e930358c6b2e56c19df75e
Alexandre Keruzec <alexandre.keruzec@gmail.com>
2018-04-24 11:57:29 +0200

    First commit

---
+++ src/main/java/org/diginamic/demo_logging/App.java
@@ -0,0 +1,16 @@
+package org.diginamic.demo_logging;
+
+import org.diginamic.service.AppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class App {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+		new AppService().executer("valeur 1");
+
+	}
+}
